b3d6153dcaa9951fd275640ba805e078b2149b91
marycriv <maryecrivelli@gmail.com>
2022-03-05 19:31:52 -0500

    hacky solution for eth_cost in coins

--- app/serializers/coin_serializer.rb
+++ app/serializers/coin_serializer.rb
@@ -2,6 +2,12 @@ class CoinSerializer
   include JSONAPI::Serializer
   attributes :pressing_id, :user_id, :for_sale, :user, :pressing
 
+  eth_price = Cryptocompare::Price.find('ETH', 'USD')['ETH']['USD'].to_f
+  
+  attribute :eth_cost do |object|
+    (eth_price * object.pressing.cost).round(2)
+  end
+
   has_one :user
   has_one :pressing
 end
